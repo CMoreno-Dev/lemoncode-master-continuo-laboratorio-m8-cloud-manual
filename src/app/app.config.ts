fbe02a203f7b8dc00976c53edef4166867f358dd
Cristian <cmoreno@uitech-group.com>
2026-04-28 11:07:11 +0200

    Add HashLocationStrategy for GitHub Pages deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { rutas } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(rutas), provideAnimations()],
+  providers: [
+    provideRouter(rutas),
+    provideAnimations(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
 };
